88259521c3a8b6ae719aa49bfd129f1f7d887da1
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-03-11 15:22:40 -0500

    W1D5 algo updated with alt method

--- Algos/W1D5.js
+++ Algos/W1D5.js
@@ -19,30 +19,52 @@
 // }
 
 function generateCoinChange(input) {
-    console.log(`Money Recieved: $${input}`);
-    var change = {"Quarters":0,"Dimes":0,"Nickels":0,"Pennies":0};
-    var centsStarting = input * 100;
-    // var centsStarting = Math.round(input * 100);
-    console.log(input, centsStarting);
-    
-    change["Quarters"] = Math.floor(centsStarting/25)
-    remainder = centsStarting%25;
-    
-    change["Dimes"] = Math.floor(remainder/10);
-    remainder = remainder%10;
-    
-    change["Nickels"] = Math.floor(remainder/5);
-    remainder = remainder%5;
-    
-    change["Pennies"] = remainder;
-    
-    for (const key in change) {
-        console.log(`${key}: ${change[key]}`);
+    console.log(`\n\nMoney Recieved: $${input}\n`); //Display user input
+    var denominations = {"Quarters":[25,0],"Dimes":[10,0],"Nickels":[5,0],"Pennies":[1,0]}; // Define denominations and counters
+
+    var cents = Math.round(input * 100); // Convert dollars to cents
+
+    for(denom in denominations){ // Iterate through denominations
+        denominations[denom][1] = Math.floor(cents/denominations[denom][0]) // Update counter for denom
+        cents = cents%denominations[denom][0]; // Update remaining cents to be converted to denom
     }
-    // console.log(change);
-    return change
+
+    console.log("Change given:"); 
+    for (const denom in denominations) { // Iterate through denominations 
+        console.log(`    ${denom}: ${denominations[denom][1]}`); // Print interpolation of denom key name and counter value
+    }
+
+    return denominations
 }
 
 generateCoinChange(3.21)
-generateCoinChange(1.40)
-generateCoinChange(20.40)
+// generateCoinChange(1.40)
+// generateCoinChange(20.40)
+
+// function generateCoinChange(input) {
+//     console.log(`Money Recieved: $${input}`);
+//     var change = {"Quarters":0,"Dimes":0,"Nickels":0,"Pennies":0};
+//     var money = Math.round(input * 100);
+//     console.log(input, centsStarting);
+    
+//     change["Quarters"] = Math.floor(centsStarting/25)
+//     remainder = centsStarting%25;
+    
+//     change["Dimes"] = Math.floor(remainder/10);
+//     remainder = remainder%10;
+    
+//     change["Nickels"] = Math.floor(remainder/5);
+//     remainder = remainder%5;
+    
+//     change["Pennies"] = remainder;
+    
+//     for (const key in change) {
+//         console.log(`${key}: ${change[key]}`);
+//     }
+//     // console.log(change);
+//     return change
+// }
+
+// generateCoinChange(3.21)
+// generateCoinChange(1.40)
+// generateCoinChange(20.40)
