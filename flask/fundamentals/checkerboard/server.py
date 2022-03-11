46a2619505e8dcae2a27961583a2aceb96886fce
Benjamin Cooke Williams <83699317+bcookew@users.noreply.github.com>
2022-03-10 23:02:23 -0500

    Fixed missing color variable

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', rows=8, columns=8, color1="white")
+    return render_template('index.html', rows=8, columns=8, color1="white", color2="black")
 
 @app.route('/<int:rows>')
 def numChecks(rows):
-    return render_template('index.html', rows=rows, columns=8, color1="white")
+    return render_template('index.html', rows=rows, columns=8, color1="white", color2="black")
 
 @app.route('/<int:rows>/<int:columns>')
 def numXY(rows, columns):
-    return render_template('index.html', rows=rows, columns=columns, color1="white")
+    return render_template('index.html', rows=rows, columns=columns, color1="white", color2="black")
 
 @app.route('/<int:rows>/<int:columns>/<color>/<color2>')
 def numXYC(rows, columns, color, color2):
